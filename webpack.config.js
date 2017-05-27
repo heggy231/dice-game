e07e408cc81838638c7f61b969de5855599985ed
jasmo2 <jasmo2.com@gmail.com>
2017-05-27 18:12:42 -0500

    refactor babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,21 @@ const webpack = require('webpack');
 
 const ROOT_DIR = path.resolve('./');
 const JS_SRC_DIR = `${ROOT_DIR}/js`;
+const devBuild = process.env.NODE_ENV !== 'production';
 
 
+if (devBuild) {
+  console.log('Webpack dev build'); // eslint-disable-line no-console
+  module.exports.devtool = 'eval-source-map';
+} else {
+  console.log('Webpack production build'); // eslint-disable-line no-console
+}
 
 module.exports = {
 	entry: {
 		app: [
+		
+			'babel-polyfill',
 			`${JS_SRC_DIR}/app.js`
 		],
 	},
@@ -36,9 +45,11 @@ module.exports = {
 
 	plugins: [
 		new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }),
+		new webpack.NamedModulesPlugin(),
 	],
 	output: {
 		path: ROOT_DIR,
 		filename: 'bundle.js'
 	}
 };
+
