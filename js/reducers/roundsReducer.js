4d3432759ddb3f0edc551e60c58d674db8203e68
jasmo2 <jasmo2.com@gmail.com>
2017-05-27 14:21:46 -0500

    Add rounds reducer

---
+++ js/reducers/roundsReducer.js
@@ -0,0 +1,12 @@
+import { TOGGLE_PLAYER } from '../constants';
+
+const INITIAL_STATE = { id: null, results: null };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case TOGGLE_PLAYER:
+      return { ...state, player: state.player === 1 ? 2 : 1 };
+    default:
+      return state;
+  }
+};
