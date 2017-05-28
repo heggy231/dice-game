fa656282117a65e8105966c4bdf9d265f706ef1c
jasmo2 <jasmo2.com@gmail.com>
2017-05-28 16:24:13 -0500

    refactor DiceContainer

--- js/containers/diceGame.js
+++ js/containers/diceGame.js
@@ -1,12 +1,33 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import DiceGame from '../components/diceGame';
+import { togglePlayer, updateScore } from '../actions';
 
+
+class DiceGameContainer extends Component {
+
+    componentWillReceiveProps(nextProps) {
+        const { dice, rounds, togglePlayer, updateScore } = nextProps; //eslint-disable-line
+        if (this.props.rounds.player === rounds.player) {
+            if (dice.number === 1) {
+                updateScore(rounds.player, 1);
+                togglePlayer();
+            } else {
+                updateScore(rounds.player, dice.number);
+            }
+        }
+    }
+
+    render() {
+        return <DiceGame {...this.props} />;
+    }
+}
+// Don't forget to actually use connect!
+// Note that we don't export Polls, but the redux "connected" version of it.
+// See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
 const mapStateToProps = (state) => {
     const { rounds, dice } = state;
     return { rounds, dice };
 };
-
-// Don't forget to actually use connect!
-// Note that we don't export Polls, but the redux "connected" version of it.
-// See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
-export default connect(mapStateToProps)(DiceGame);
+const mapDistapchToProps = { togglePlayer, updateScore };
+export default connect(mapStateToProps, mapDistapchToProps)(DiceGameContainer);
--- js/reducers/roundsReducer.js
+++ js/reducers/roundsReducer.js
@@ -1,11 +1,11 @@
 import { TOGGLE_PLAYER } from '../constants';
 
-const INITIAL_STATE = { id: null, results: null };
+const INITIAL_STATE = { player: 0 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case TOGGLE_PLAYER:
-      return { ...state, player: state.payload === 0 ? 1 : 0 };
+      return { ...state, player: state.player === 0 ? 1 : 0 };
     default:
       return state;
   }
