8c0afa547f7612b2a6b1f0696390de2c5ddc4d11
jasmo2 <jasmo2.com@gmail.com>
2017-05-29 16:46:22 -0500

    separate prop-types separeta dependency, deprecation warning

--- js/components/diceGame.js
+++ js/components/diceGame.js
@@ -1,4 +1,5 @@
-import React,{ PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Players from '../containers/players';
 import RollDiceButton from '../containers/roleDiceButton';
 import HoldButton from '../containers/holdButton';
