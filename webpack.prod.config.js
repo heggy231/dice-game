3123e459808d3b60e1e31119788211e27f272fcc
jasmo2 <jasmo2.com@gmail.com>
2017-05-28 20:39:59 -0500

    new prod config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -44,7 +44,11 @@ module.exports = {
 	},
 
 	plugins: [
-		new webpack.optimize.DedupePlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
 		new webpack.optimize.UglifyJsPlugin({
 			minimize: true,
 			compress: { warnings: false }
