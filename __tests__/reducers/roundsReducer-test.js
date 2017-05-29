94b95b58087aa11e83c34a9aeae2f8f096e748ca
jasmo2 <jasmo2.com@gmail.com>
2017-05-29 10:49:10 -0500

    update dependencies for testing

--- __tests__/reducers/roundsReducer-test.js
+++ __tests__/reducers/roundsReducer-test.js
@@ -6,12 +6,12 @@ const myInitialState = {
 };
 
 describe('Rounds reducer test', () => {
-    it('update the current player from 1 to 2', () => {
+    it('update the current player from 0 to 1', () => {
         const state = RoundsReducer(myInitialState, { type: TOGGLE_PLAYER });
-        expect(state).toEqual({ player: 2 });
-    });
-    it('update the current player from 2 to 1', () => {
-        const state = RoundsReducer({ player: 2 }, { type: TOGGLE_PLAYER });
         expect(state).toEqual({ player: 1 });
+    });
+    it('update the current player from 1 to 0', () => {
+        const state = RoundsReducer({ player: 1 }, { type: TOGGLE_PLAYER });
+        expect(state).toEqual({ player: 0 });
     });
 });
