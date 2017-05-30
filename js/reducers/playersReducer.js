2aa04f1b53685cb8899a9c6349e71f75856fa336
jasmo2 <jasmo2.com@gmail.com>
2017-05-29 20:08:28 -0500

    handle user refresing in explorer

--- js/reducers/playersReducer.js
+++ js/reducers/playersReducer.js
@@ -1,6 +1,6 @@
 import { RESET_PLAYERS, UPDATE_SCORE, UPDATE_GLOBAL_SCORE } from '../constants';
 
-const INITIAL_STATE = [
+const INITIAL_STATE = localStorage.getItem('players') ? JSON.parse(localStorage.getItem('players')) : [
   { id: 0, currentScore: 0, totalScore: 0 },
   { id: 1, currentScore: 0, totalScore: 0 }
 ];
@@ -19,21 +19,30 @@ export default (state = INITIAL_STATE, action) => {
       // if (!!currentScore){ return state } //eslint-disable-line
       currentPlayer.currentScore = currentScore !== 1 ? 
                                    currentPlayer.currentScore + currentScore : 0;
-      return state.map((player) => (
+      const players = state.map((player) => (
         player.id === action.id ? currentPlayer : player
       ));
+      localStorage.setItem('players', JSON.stringify(players));
+      return players;
     }
     case UPDATE_GLOBAL_SCORE: {
       const { id } = action.payload;
       const currentPlayer = getCurrentPlayer(id, state);
       currentPlayer.totalScore += currentPlayer.currentScore;
       currentPlayer.currentScore = 0;
-      return state.map((player) => (
+      const players = state.map((player) => (
         player.id === action.id ? currentPlayer : player
       ));
+      localStorage.setItem('players', JSON.stringify(players));
+      
+      return players
     }
     case RESET_PLAYERS: 
-      return INITIAL_STATE;
+      localStorage.removeItem('players');    
+      return [
+        { id: 0, currentScore: 0, totalScore: 0 },
+        { id: 1, currentScore: 0, totalScore: 0 }
+      ];
     default:
       return state;
   }
